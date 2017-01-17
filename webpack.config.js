84a921784f65ba056d52edf8ce41d44a36d3551f
Dave Irvine <dave@daveirvine.me>
2017-01-17 13:32:50 +0000

    Add semantic-ui and make webpack load

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,53 @@
+var autoprefixer = require('autoprefixer');
+
 module.exports = {
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
-    },
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
 
-    devServer: {
-        inline: true,
-        port: 7777,
-        contentBase: __dirname + '/public'
-    },
+  devServer: {
+    inline: true,
+    port: 7777,
+    contentBase: __dirname + '/public'
+  },
 
-    module:
-    {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'stage-0', 'react']
-                }
-            }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?importLoaders=1!postcss'
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        loader: 'file',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+    ],
+  },
+  postcss: function() {
+    return [
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
         ]
-    }
+      }),
+    ];
+  },
 };
