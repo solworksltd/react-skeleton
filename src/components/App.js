ad5e6cfcca5fc2fb4a2498d2303024851690bdda
Dave Irvine <dave@daveirvine.me>
2017-01-17 13:06:50 +0000

    ESLint fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
-    render(){
-
-        return (
-                <h1>Hello React Skeleton</h1>
-        );
-    }
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <h1>Hello React Skeleton</h1>
+    );
+  }
 }
 
 export default App;
